93ed4653aaddc0403720a574b2d79cbfda2282b9
chizzy01 <elvischizzy09@gmail.com>
2021-10-04 19:52:33 +0100

    my first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import { Container } from '@material-ui/core';
+import  { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     welcome back
     </div>
   );
 }
 
 export default App;
+
+
+// Api=https://opentdb.com/api.php?amount=10&category=9&difficulty=medium&type=multiple
